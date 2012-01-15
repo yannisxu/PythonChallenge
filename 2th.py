419b7059ddca3e39cbd32ef71152001720b2be09
Yannis <excellentbtight@gmail.com>
2012-01-16 06:24:40 +0800

    finish the 2th.py

--- 2th.py
+++ 2th.py
@@ -1,2 +1,9 @@
 #3th
-print "hello";
+filter="abcdefghijklmnopqrstuvwxyz";
+fileHandle = open('sources/2.txt');
+string = fileHandle.read();
+for char in filter:
+    if(string.count(char)>0):
+        print char;
+
+
