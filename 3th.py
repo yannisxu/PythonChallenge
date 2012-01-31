ff68133362e9534a7f0b506bebb5c379ffeb01dc
Yannis <excellentbtight@gmail.com>
2012-01-31 21:49:34 +0800

    3th upload

--- 3th.py
+++ 3th.py
@@ -10,9 +10,13 @@
 ##                
 ######################################################################
 
+#答案linkedlist
+
 import re 
 
 f=open("sources/3.txt","r")
 text=f.read()
 text.strip()
-text
+
+p=re.compile(r'[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]')
+print p.findall(text)
