84f5fef3b9b6fea896bf5a85933a5d592cffbfd5
Yannis <excellentbtight@gmail.com>
2012-01-31 13:35:22 +0800

    add 3th source re

---
+++ 3th.py
@@ -0,0 +1,18 @@
+######################################################################
+## Filename:      3th.py
+##                
+## Copyright (c) 2012,Yannis
+## Version:       
+## Author:        Yannis.Xu <excellentbright@gmail.com>
+## Created at:    Fri Jan 27 15:28:23 2012
+##                
+## Description:   
+##                
+######################################################################
+
+import re 
+
+f=open("sources/3.txt","r")
+text=f.read()
+text.strip()
+text
