b012e35af10a2abaf12af5147ddca137cf6f6876
yannis <yannis@yannis-Rev-1-0.(none)>
2012-02-29 07:50:18 +0800

    add 10th(not finished)

---
+++ 10th.py
@@ -0,0 +1,40 @@
+######################################################################
+## Filename:      10th.py
+##                
+## Copyright (c) 2012,Yannis
+## Version:       
+## Author:        Yannis.Xu <excellentbright@gmail.com>
+## Created at:    Wed Feb 29 06:51:40 2012
+##                
+## Description:   
+##                
+######################################################################
+
+#查看源代码，根据提示 打开sequence.txt, 看到的结果是 a=[1, 11, 21, 1211, 111221 , 根据bull.html页面的提示 len(a[30]) 求解
+#数字的规律是后一个描述前一个，比如 11指前面1个1，21是指前面2个1，1211是指前面1个2，1个1，111221指前面1个1，1个2，2个1，根据提示得出312211
+
+import string
+
+startstr = "1"
+temp = ""
+count = 1
+newstr = ""
+
+for i in range(30):
+    for j in startstr:
+        if temp == j:
+            count += 1
+        else:
+            temp = j
+            newstr += str(count)
+            newstr += temp
+            count = 0
+            temp = j
+
+    if count > 0:
+        newstr += str(count)
+        newstr += temp
+            
+    print "第 %d 次循环" %(i)
+    print newstr
+    startstr = newstr
